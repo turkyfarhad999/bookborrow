6fd9c94bbc4f9b903913653dee4fc1385c434360
Farhad Ahmmed Turky <turkyfarhad@yahoo.com>
2026-07-10 19:45:40 +0600

    tt

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -5,56 +5,77 @@ import Image from 'next/image';
 import logo from '@/assets/logobooktop.png'
 import { auth } from '@/lib/auth';
 import { headers } from 'next/headers';
-import { authClient } from '@/lib/auth-client';
 import Logoutbtn from '../logoutbtn/Logoutbtn';
 
-const Navbar = async() => {
-   const session = await auth.api.getSession({
+const Navbar = async () => {
+    const session = await auth.api.getSession({
         headers: await headers()
     })
-   
-    const link=<>
-  <Navlink href={'/'}>Home</Navlink>
-  <Navlink href={'/books'}>Books</Navlink>
-  <Navlink href={'/profile'}>Profile</Navlink>
-   
-    </>
+
     return (
         <div>
             <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 text-black rounded-box z-1 mt-3 w-52 p-2 shadow">
-        {link}
-      </ul>
-    </div>
-   <Link className='ml-3' href={'/'}>
-   <Image
-   src={logo}
-   width={25}
-   height={25}
-   alt='logo'/>
-   </Link>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1 text-black font-semibold space-x-5 ">
-       {link}
-    </ul>
-  </div>
-  <div className="navbar-end space-x-2 ">
-    <p className='text-black'>Welcome <span className='font-semibold'> {session&& `${session.user.name}`}</span></p>
-    {session ? (
-  <Logoutbtn></Logoutbtn>
-) : (
-  <Link href="/login" className="btn">Login</Link>
-)}
-  </div>
-</div>
+
+                {/* ---------- navbar-start ---------- */}
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 text-black rounded-box z-1 mt-3 w-52 p-2 shadow"
+                        >
+                            <li><Navlink href={'/'}>Home</Navlink></li>
+                            <li><Navlink href={'/books'}>Books</Navlink></li>
+                            <li><Navlink href={'/profile'}>Profile</Navlink></li>
+                        </ul>
+                    </div>
+
+                    <Link className="ml-3" href={'/'}>
+                        <Image
+                            src={logo}
+                            width={25}
+                            height={25}
+                            alt="logo"
+                        />
+                    </Link>
+                </div>
+
+                {/* ---------- navbar-center ---------- */}
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 text-black font-semibold space-x-5">
+                        <li><Navlink href={'/'}>Home</Navlink></li>
+                        <li><Navlink href={'/books'}>Books</Navlink></li>
+                        <li><Navlink href={'/profile'}>Profile</Navlink></li>
+                    </ul>
+                </div>
+
+                {/* ---------- navbar-end ---------- */}
+                <div className="navbar-end space-x-2">
+                    <p className="text-black">
+                        Welcome <span className="font-semibold">{session && `${session.user.name}`}</span>
+                    </p>
+                    {session ? (
+                        <Logoutbtn></Logoutbtn>
+                    ) : (
+                        <Link href="/login" className="btn">Login</Link>
+                    )}
+                </div>
+            </div>
         </div>
     );
 };
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -50,7 +50,7 @@ const signInGit = async () => {
         <form onSubmit={handleSubmit(onSubmit)} className="bg-white">
         
     <div className="hero bg-base-200 min-h-screen ">
-  <div className="hero-content flex-col lg:flex-row-reverse w-[600px]">
+  <div className="hero-content flex-col lg:flex-row-reverse ">
     
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body text-black">
