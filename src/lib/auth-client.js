df534742c95f492bc56195275e85df06627b9082
Farhad Ahmmed Turky <turkyfarhad@yahoo.com>
2026-07-10 19:28:29 +0600

    tt

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,7 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from 'better-auth/react';
+
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
-})
-export const { signIn, signUp, useSession } = createAuthClient()
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
+});
+
+export const { signIn, signUp, useSession } = authClient;
